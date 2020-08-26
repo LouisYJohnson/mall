a26bcd50e5bd5329f814ebc4f8b1af395f30f5bd
LouisYJohnson <623988524@qq.com>
2020-08-26 16:11:09 +0800

    new codes

--- src/main/java/com/ibook/mall/dao/ProductMapper.java
+++ src/main/java/com/ibook/mall/dao/ProductMapper.java
@@ -21,6 +21,9 @@ public interface ProductMapper {
 
     //一旦mybatis中传入的数据类型不是一般的数据类型,需要单独加注解
     //方法目的:传入一个产品的id将其相关(所有子类类目的商品全都查出来,拿着1级查2级,不可能是递归,别和ServiceImpl中的搞混了)
+    //https://blog.csdn.net/mrqiang9001/article/details/79520436
+    //有了@Param注解,在xml中不用写parameterType,直接用注解中的别名就可以了,一般是在传多个参数的时候用
+    //这里只是展示一下
     List<Product> selectByCategoryIdSet(@Param("categoryIdSet") Set<Integer> categoryIdSet);
 
     List<Product> selectByProductIdSet(@Param("categoryIdSet") Set<Integer> productIdSet);
